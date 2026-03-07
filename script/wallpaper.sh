6c3f1b54608f9d27ff6aa8f9a568e5d756d3b111
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-03-07 15:03:30 +0100

    ignore nvim genned colors, walpaper presets folderable

--- script/wallpaper.sh
+++ script/wallpaper.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source "$HOME/.local/share/scale-shell/vars.sh"
+source "$SCALEH/get-timestamp.sh"
 
 if [[ $# -lt 1 ]]; then
 	echo "Usage: bash wallpaper.sh <random|preset NUM|hook>"
@@ -11,14 +12,42 @@ get-active-monitor() {
 	niri msg focused-output | grep -Po "Output.*\\(\\K[^\\)]+"
 }
 
+get_random_file_or_return_path() {
+	local path="$1"
+	# Check if the path is a directory
+	if [[ -d "$path" ]]; then
+		# Get a random file from the directory
+		local files=("$path"/*) # Array of files in the directory
+		if [[ ${#files[@]} -eq 0 ]]; then
+			echo "No files found in directory: $path"
+			return 1
+		fi
+		local random_file=${files[RANDOM % ${#files[@]}]} # Pick a random file
+		echo "$random_file"
+	else
+		echo "$path"
+	fi
+}
+
 temp_name="SCALE_TEMP_BACKGROUND_"
 if [[ $1 == "random" ]]; then
-	rm "$SCALEW/$temp_name"*
+	# Remove old temp files
+	rm "$SCALEW/$temp_name"* &>/dev/null || true
 	qs -c noctalia-shell ipc call wallpaper random "$(get-active-monitor)"
 elif [[ $1 == "preset" ]]; then
-	rm "$SCALEW/$temp_name"*
-	cp "$SCALEWP/$2."* "$SCALEW/$temp_name$2" || exit 1
-	qs -c noctalia-shell ipc call wallpaper set "$SCALEW/$temp_name$2" "$(get-active-monitor)"
+	# Remove old temp files
+	rm "$SCALEW/$temp_name"* &>/dev/null || true
+	chosen_preset="$(get_random_file_or_return_path "$SCALEWP/$2")"
+	# Ensure different name for new background, else noctalia ignores
+	target="$SCALEW/$temp_name$SCALESTAMP"
+	# Have to put them in the wallpaper directory first
+	if [[ -e "$chosen_preset" ]]; then
+		cp "$chosen_preset" "$target"
+	else
+		# Fallback to handle single files with extensions
+		cp "$chosen_preset."* "$target"
+	fi
+	qs -c noctalia-shell ipc call wallpaper set "$target" "$(get-active-monitor)"
 elif [[ $1 == "hook" ]]; then
 	# Refresh gnome stuff to dark theme
 	gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
