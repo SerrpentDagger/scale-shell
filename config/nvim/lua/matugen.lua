2b0876a3277bd98797f1c9240bfc9819db8ad3b8
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-03-07 13:18:33 +0100

    Forgot to pull configs prev commit

--- config/nvim/lua/matugen.lua
+++ config/nvim/lua/matugen.lua
@@ -3,23 +3,23 @@ local M = {}
 function M.setup()
   require("base16-colorscheme").setup({
     -- Background tones
-    base00 = "#142829", -- Default Background
-    base01 = "#214345", -- Lighter Background (status bars)
-    base02 = "#1e3c3e", -- Selection Background
-    base03 = "#5d726f", -- Comments, Invisibles
+    base00 = "#033a23", -- Default Background
+    base01 = "#0f5738", -- Lighter Background (status bars)
+    base02 = "#0b5033", -- Selection Background
+    base03 = "#698076", -- Comments, Invisibles
     -- Foreground tones
-    base04 = "#afb6b5", -- Dark Foreground (status bars)
-    base05 = "#f2f3f3", -- Default Foreground
-    base06 = "#f2f3f3", -- Light Foreground
-    base07 = "#f2f3f3", -- Lightest Foreground
+    base04 = "#afb6b3", -- Dark Foreground (status bars)
+    base05 = "#f2f3f2", -- Default Foreground
+    base06 = "#f2f3f2", -- Light Foreground
+    base07 = "#f2f3f2", -- Lightest Foreground
     -- Accent colors
     base08 = "#fd4663", -- Variables, XML Tags, Errors
-    base09 = "#a2cc66", -- Integers, Constants
-    base0A = "#5ca8d6", -- Classes, Search Background
-    base0B = "#67e4d5", -- Strings, Diff Inserted
-    base0C = "#c7e996", -- Regex, Escape Chars
-    base0D = "#93ece1", -- Functions, Methods
-    base0E = "#96c9e9", -- Keywords, Storage
+    base09 = "#3d8bf5", -- Integers, Constants
+    base0A = "#3de7f5", -- Classes, Search Background
+    base0B = "#55f6b2", -- Strings, Diff Inserted
+    base0C = "#86b6f9", -- Regex, Escape Chars
+    base0D = "#86f9c8", -- Functions, Methods
+    base0E = "#86f0f9", -- Keywords, Storage
     base0F = "#900017", -- Deprecated, Embedded Tags
   })
 end
