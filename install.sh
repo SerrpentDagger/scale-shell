f983b3117ad735745d6978407890aaafe577e9d2
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-03-16 22:04:50 +0100

    type fix

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ gum confirm "Setup .desktop entries?" && source "$SCALES/desktops.sh" || true
 
 gum confirm "Install Mullvad Browser?" && source "$SCALES/mullvad.sh" || true
 gum confirm "Install Tor Browser?" && source "$SCALES/mullvad.sh" tor || true
-gum canfirm "Install Brave browser?" && source "$SCALES/brave.sh" || true
+gum confirm "Install Brave browser?" && source "$SCALES/brave.sh" || true
 gum confirm "Install VintageStory? (Semi-closed, requires paid account, flatpak)" && source "$SCALES/vintagestory.sh" || true
 
 gum confirm "Setup Alacritty context menu entry for Nautilus?" && source "$SCALES/gnome-terminal.sh" || true
