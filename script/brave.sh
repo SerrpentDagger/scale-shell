81b62f3218cb4ae00d5b71a8a7eef507120d6de0
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-03-16 22:02:40 +0100

    maybefixed brave

--- script/brave.sh
+++ script/brave.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+source "$SCALEH/tmp-spawn.sh"
+cd "$SCALET" || exit 1
 curl -fsSLO "https://dl.brave.com/install.sh{,.asc}" && gpg --keyserver hkps://keys.openpgp.org --recv-keys D16166072CACDF2C9429CBF11BF41E37D039F691 && gpg --verify install.sh.asc
+source "$SCALEH/tmp-clear.sh"
