4d4ccbaf5bb096303bbed431610b39fa2c6661c4
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-04-02 23:37:58 +0200

    tlp config added, balena to list

--- script/configs.sh
+++ script/configs.sh
@@ -14,6 +14,7 @@ mv ~/.config/alacritty/alacritty.toml "$feather_backup/alacritty.toml"
 mkdir "$feather_backup/qt"
 mv ~/.config/qt5ct/qt5ct.conf "$feather_backup/qt"
 mv ~/.config/qt6ct/qt6ct.conf "$feather_backup/qt"
+sudo mv /etc/tlp.conf "$feather_backup/tlp.conf"
 
 cp -R niri ~/.config/niri
 cp -R noctalia ~/.config/noctalia
@@ -21,6 +22,7 @@ cp -R nvim ~/.config/nvim
 cp alacritty/alacritty.toml ~/.config/alacritty/
 cp qt/qt6ct.conf ~/.config/qt6ct/
 cp qt/qt5ct.conf ~/.config/qt5ct/
+sudo cp tlp.conf /etc/tlp.conf
 
 sed_string="s|/home/serpent|/home/$USER|g"
 sed -i "$sed_string" ~/.config/noctalia/settings.json
