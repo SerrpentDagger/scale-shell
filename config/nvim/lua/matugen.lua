974d7d3cfad7fa1cf6b5007e1d6594951b5f20ae
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-04-02 22:27:21 +0200

    tablet-to-hdmi hot rule and binding

--- config/nvim/lua/matugen.lua
+++ config/nvim/lua/matugen.lua
@@ -3,10 +3,10 @@ local M = {}
 function M.setup()
   require("base16-colorscheme").setup({
     -- Background tones
-    base00 = "#111123", -- Default Background
-    base01 = "#212245", -- Lighter Background (status bars)
-    base02 = "#1e1f3e", -- Selection Background
-    base03 = "#626277", -- Comments, Invisibles
+    base00 = "#090a2a", -- Default Background
+    base01 = "#121454", -- Lighter Background (status bars)
+    base02 = "#10124c", -- Selection Background
+    base03 = "#5f5f73", -- Comments, Invisibles
     -- Foreground tones
     base04 = "#afafb6", -- Dark Foreground (status bars)
     base05 = "#f2f2f3", -- Default Foreground
@@ -15,10 +15,10 @@ function M.setup()
     -- Accent colors
     base08 = "#fd4663", -- Variables, XML Tags, Errors
     base09 = "#f1ec5a", -- Integers, Constants
-    base0A = "#955cd6", -- Classes, Search Background
-    base0B = "#b6ec7a", -- Strings, Diff Inserted
-    base0C = "#83e581", -- Regex, Escape Chars
-    base0D = "#c966cc", -- Functions, Methods
+    base0A = "#e7baee", -- Classes, Search Background
+    base0B = "#36d21d", -- Strings, Diff Inserted
+    base0C = "#7aef75", -- Regex, Escape Chars
+    base0D = "#d757db", -- Functions, Methods
     base0E = "#676be4", -- Keywords, Storage
     base0F = "#900017", -- Deprecated, Embedded Tags
   })
@@ -27,25 +27,25 @@ end
 primary: #676be4
 on_primary: #181925
 primary_container: #0c108d
-on_primary_container: #dcddef
+on_primary_container: #d5d6f6
 primary_fixed: #bec0f4
 primary_fixed_dim: #9396ec
 on_primary_fixed: #21212c
 on_primary_fixed_variant: #2b2c3b
 
-secondary: #955cd6
+secondary: #e7baee
 on_secondary: #181925
-secondary_container: #3d106f
-on_secondary_container: #e5dcef
-secondary_fixed: #d7bef4
-secondary_fixed_dim: #bd96e9
-on_secondary_fixed: #26212c
-on_secondary_fixed_variant: #322b3b
+secondary_container: #be1ed7
+on_secondary_container: #fbf6fb
+secondary_fixed: #ecbef4
+secondary_fixed_dim: #de96e9
+on_secondary_fixed: #2a212c
+on_secondary_fixed_variant: #392b3b
 
-tertiary: #c966cc
+tertiary: #d757db
 on_tertiary: #181925
-tertiary_container: #671669
-on_tertiary_container: #eedcef
+tertiary_container: #6f0d73
+on_tertiary_container: #f5d5f6
 tertiary_fixed: #f2bef4
 tertiary_fixed_dim: #e696e9
 on_tertiary_fixed: #2c212c
@@ -56,34 +56,34 @@ on_error: #181925
 error_container: #900017
 on_error_container: #fecdd4
 
-surface: #14142a
+surface: #0b0c32
 on_surface: #f2f2f3
-surface_variant: #1a1b37
+surface_variant: #0e1043
 on_surface_variant: #afafb6
-surface_dim: #0d0e1c
-surface_bright: #272953
-surface_container_lowest: #0a0a15
-surface_container_low: #101123
-surface_container: #212245
-surface_container_high: #1e1f3e
-surface_container_highest: #24254c
+surface_dim: #070822
+surface_bright: #161865
+surface_container_lowest: #050619
+surface_container_low: #090a2a
+surface_container: #121454
+surface_container_high: #10124c
+surface_container_highest: #14165c
 
-outline: #626277
-outline_variant: #626277
-shadow: #14142a
+outline: #5f5f73
+outline_variant: #5f5f73
+shadow: #0b0c32
 scrim: #000000
 
 inverse_surface: #e3e4e8
 inverse_on_surface: #242428
-inverse_primary: #333699
+inverse_primary: #31349b
 
-background: #14142a
+background: #0b0c32
 on_background: #f2f2f3
 
 hued: #e4cf67
 invert: #98941b
-invert: #6aa329
-invert: #369933
+invert: #184511
+invert: #28a824
 --]]
 -- Register a signal handler for SIGUSR1 (matugen updates)
 local signal = vim.uv.new_signal()
