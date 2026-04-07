ab46959d99ff63caeaabf9231abb8623f6ad4790
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-04-07 23:15:47 +0200

    Fix gnome-terminal.sh query fail, remove unnecessary packages from list.

--- script/gnome-terminal.sh
+++ script/gnome-terminal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Install and setup alacritty terminal in nautilus context menu"
-yay -S nautilus-open-any-terminal
+yay -Sua --noconfirm nautilus-open-any-terminal
 nautilus -q
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas
 gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal alacritty
