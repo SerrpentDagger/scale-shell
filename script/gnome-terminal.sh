8af50a419c60c7830a822fbf620058f887c772a1
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-04-13 01:43:41 +0200

    Fix nautilus not open error, malformed wallpaper lines

--- script/gnome-terminal.sh
+++ script/gnome-terminal.sh
@@ -2,7 +2,7 @@
 
 echo "Install and setup alacritty terminal in nautilus context menu"
 yay -Sua --noconfirm nautilus-open-any-terminal
-nautilus -q
+nautilus -q || true
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas || echo "Warning: Failed to compile glib schemas!"
 if gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal alacritty; then
 	source "$FEATHERH/sel-comps.sh" --pending remove "Alacritty in Nautilus"
